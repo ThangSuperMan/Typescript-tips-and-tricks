632046fe928f62cb73da0a3f47a9c2e4a4a77442
thangsuperman <thangjenny2002@gmail.com>
2023-06-20 14:14:07 +0700

    Add prettify class utility for support speard fields of interfaces no matter how it nested(good for debug)

---
+++ prettify-class/index.ts
@@ -0,0 +1,42 @@
+interface Person {
+  name: string;
+  age: number;
+}
+
+type Prettify<T> = {
+  [P in keyof T]: T[P];
+} & {};
+
+console.log("prettify :>> ");
+
+const stranger: Person = {
+  name: "Thang",
+  age: 21,
+};
+
+console.log("stranger :>> ", stranger);
+
+interface Enginner extends Person {
+  role: string;
+}
+
+interface FrontendEnginner extends Enginner {
+  title: string;
+}
+
+const enginnerOne: Enginner = {
+  name: "Trang",
+  age: 21,
+  role: "admin",
+};
+
+console.log("enginnerOne :>> ", enginnerOne);
+
+const frontend: Prettify<FrontendEnginner> = {
+  name: "Kevin",
+  title: "Intern",
+  age: 22,
+  role: "developer",
+};
+
+console.log("frontend :>> ", frontend);
